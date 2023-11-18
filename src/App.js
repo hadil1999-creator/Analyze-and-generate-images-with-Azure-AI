8fb46d233a8235ab70898a6a38094c379ad5f692
hadil1999-creator <benamor.hadil@supcom.tn>
2023-11-18 22:12:12 +0100

    add title

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function App() {
   const value = 'World';
+  const TITLE = 'My Page Title';
+
   return <div>Hello {value}</div>;
+  return (
+    
+      <Helmet>
+        <title>{ TITLE }</title>
+      </Helmet> )
 }
 
 export default App;
